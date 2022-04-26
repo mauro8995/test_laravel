6ec0bf51f8dd5d07877c9926e4eaaf1e54fc7345
Mauro Jhony Gomez Lino <mauro.gomez.lino.94@gmail.com>
2022-04-25 19:13:16 -0500

    primer comit subir ptoyecto

---
+++ app/Http/Livewire/Users.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\User;
+
+use Illuminate\Support\Facades\Validator;
+
+
+class Users extends Component
+{
+
+    public $search;
+
+    public $name;
+    public $password;
+    public $email;
+    public $id_user;
+
+    protected $rules = [
+        'name' => 'required|min:6',
+        'email' => 'required|email',
+        'password' =>'required|min:6|
+        regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/|
+        confirmed'
+    ];
+
+    //pagina de inicio usuario
+    public function render()
+    {
+        return view('livewire.users',["usuarios"=>
+        User::where('name','LIKE','%'.$this->search.'%')->get()
+        ]);
+    }
+
+    //eliminar usuario
+    public function eliminar($id){
+        User::find($id)->delete();
+    }
+
+    //limpar datos
+    public function limpiar(){
+        $this->name = "";
+        $this->email = "";
+        $this->id_user = "";
+        $this->password = "";
+    }
+
+    //Crear usuario
+    public function crear(){
+
+        $this->validate();
+
+        $u = new User();
+        $u->name = $this->name;
+        $u->email = $this->email;
+        $u->password= bcrypt($this->password);
+        $u->save();
+    }
+
+    //obtener usuario
+    public function get_user($id){
+        $u = User::find($id);
+        $this->name = $u->name;
+        $this->email = $u->email;
+        $this->id_user = $u->id;
+    }
+
+    //actualizar usuario
+    public function actualizar(){
+        $this->validate();
+        $u = User::find($this->id_user);
+        $u->name = $this->name ;
+        $u->email = $this->email;
+        $u->password= bcrypt($this->password);
+        $u->save();
+
+        return redirect('/usuarios');
+    }
+
+}
---
+++ resources/views/livewire/users.blade.php
@@ -0,0 +1,136 @@
+<div>
+    {{-- The whole world belongs to you. --}}
+
+    <div class="container">
+        <div class="row">
+
+                @error('name')
+                    <div class="alert alert-danger">
+                        <span class="error">{{ $message }}</span>
+                    </div>
+                @enderror
+
+
+        </div>
+        <div class="row">
+
+                <div class="col align-self-start">
+                    <label>
+                        Buscar usuarios
+                        <input wire:model="search" type="search" />
+                    </label>
+                </div>
+              <div class="col align-self-center">
+
+              </div>
+              <div class="col align-self-end">
+
+                <button type="button" wire:click="limpiar()" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    crear
+                  </button>
+                <!-- inicio Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" wire:ignore.self>
+
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">CREAR USUARIO</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form>
+                                                <div class="mb-3">
+                                                    <label for="disabledTextInput" class="form-label">Nombres</label>
+                                                    <input type="text" wire:model="name" name="name" id="name" class="form-control" placeholder="Nombres">
+
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="disabledTextInput" class="form-label">correo</label>
+                                                    <input type="text" wire:model="email" name="email" id="email" class="form-control" placeholder="correo">
+                                                </div>
+
+                                                <div class="mb-3">
+                                                    <label for="disabledTextInput" class="form-label">contraseña</label>
+                                                    <input type="password" wire:model="password" name="password" id="password" class="form-control" placeholder="contraseña">
+                                                </div>
+
+                                                <button   wire:click="crear()" data-bs-dismiss="modal" class="btn btn-success">Crear</button>
+                                            </form>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                <!-- fin Modal -->
+
+                  <!-- inicio Modal -->
+                  <div class="modal fade" id="editarModal" tabindex="-1" aria-labelledby="editarModal" aria-hidden="true" wire:ignore.self>
+
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h5 class="modal-title" id="editar">EDITAR USUARIO</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form >
+                                    <div class="mb-3">
+                                        <label for="disabledTextInput" class="form-label">Nombres</label>
+                                        <input type="text" wire:model="name" name="name" id="name" class="form-control" placeholder="Nombres">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="disabledTextInput" class="form-label">correo</label>
+                                        <input type="text" wire:model="email" name="email" id="email" class="form-control" placeholder="correo">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="disabledTextInput" class="form-label">contraseña</label>
+                                        <input type="password" wire:model="password" name="password" id="password" class="form-control" placeholder="contraseña">
+                                    </div>
+
+                                    <button type="submit"  wire:click="actualizar()" class="btn btn-success">Editar</button>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- fin Modal -->
+
+              </div>
+        </div>
+    </div>
+
+
+    <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">NOMBRES</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">ACCIÓN</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($usuarios as $item)
+          <tr>
+
+            <td>{{$item->name}}</td>
+            <td>{{$item->email}}</td>
+            <td>
+                <button wire:click="eliminar({{$item->id}})" class="btn btn-danger">Eliminar</button>
+                <button wire:click="get_user({{$item->id}})" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editarModal">Editar</button>
+            </td>
+
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+
+
+
+
+</div>
